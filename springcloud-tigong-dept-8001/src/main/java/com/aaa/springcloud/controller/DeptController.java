256ce2be248cb8ef79d024565e7c8c90502bf37d
tugege1 <2323723533@qq.com>
2021-09-02 11:50:41 +0800

    添加API,获取微服务信息

--- springcloud-tigong-dept-8001/src/main/java/com/aaa/springcloud/controller/DeptController.java
+++ springcloud-tigong-dept-8001/src/main/java/com/aaa/springcloud/controller/DeptController.java
@@ -4,6 +4,8 @@ import com.aaa.springcloud.entity.Dept;
 import com.aaa.springcloud.service.DeptService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+    //注入这个类，使用它提供的方法
+    @Autowired
+    private DiscoveryClient client;
 
     //查询所有部门
     @GetMapping
@@ -36,5 +41,27 @@ public class DeptController {
         boolean insert = deptService.addDept(dept);
         return insert == true ? "成功" : "失败";
     }
+
+    //新增一个Api,用来获取微服务信息
+    @GetMapping("/other/discovery")
+    public Object discovery() {
+        //获得微服务列表
+        List<String> services = client.getServices();
+        System.out.println("discovery=>services:" + services);
+        //再通过一个具体的微服务id，获取到关于它的所有信息
+        List<ServiceInstance> instance = client.getInstances("springcloud-tigong-ip");
+        //最后foreach遍历出信息
+        for (ServiceInstance tigong : instance) {
+            System.out.println(
+                    tigong.getHost() + "\t" + // 主机名称
+                            tigong.getPort() + "\t" + // 端口号
+                            tigong.getUri() + "\t" + // uri
+                            tigong.getServiceId() // 服务id
+            );
+        }
+        //object类型，不是String类型
+        return this.client;
+    }
+
 
 }
